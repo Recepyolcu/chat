17727a501f077223351a953583da08f4863b2913
Recep Yolcu <reeceep05@gmail.com>
2023-02-14 18:25:56 +0300

    --no-edit

---
+++ public/chat.js
@@ -0,0 +1,42 @@
+const socket = io.connect('192.168.1.125/pearl/public/');
+
+const sender = document.getElementById('sender');
+const user2PP = document.getElementById('profile-photo');
+
+const output = document.getElementById('output');
+
+const feedback = document.getElementById('feedback');
+const message = document.getElementById('message');
+const submitBtn = document.getElementById('submit-btn');
+
+// uygulamadan önce sender ı al
+
+
+submitBtn.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: message.value
+    });
+
+    if (message.value == '') {
+
+    }
+    else {
+        message.value = '';
+    }
+});
+
+message.addEventListener('keypress', () => {
+    socket.emit('typing');
+});
+
+let userBox;
+socket.on('divCreated', (data) => {
+    feedback.innerHTML = '';
+
+    userBox = data;
+    output.innerHTML += userBox;
+});
+
+socket.on('typing', () => {
+    feedback.innerHTML = 'typing...';
+});
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+const server = app.listen(3000);
+
+app.use(express.static('public'));
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    
+    socket.on('chat', (data) => {
+        const address = socket.handshake.address; 
+        
+        console.log(address);
+        
+        // if (user == '::1') {
+        //     const bubble = '<div class="chat-bubble"><p>' + data.message + '</p><div>';
+        //     const div = '<div class="user-box">' + bubble + '<div>';
+        //     data = div;
+        //     io.sockets.emit('divCreated', data);
+        // }
+        // else {
+        //     const bubble = '<div class="chat-bubble"><p>' + data.message + '</p><div>';
+        //     const div = '<div class="client-box">' + bubble + '<div>';
+        //     data = div;
+        //     io.sockets.emit('divCreated', data);
+        // }
+        
+       
+    });
+
+    socket.on('typing', () => {
+        socket.broadcast.emit('typing');
+    });
+});
